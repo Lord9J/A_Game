12677b7fbb4aab689d9dad9877359ccca51ad16b
Lord9J <Lord9J@gmail.com>
2021-12-08 15:04:18 +0500

    Добавил глобальную переменную [Globals.gd]

---
+++ Basic Top Down Shooter/UI/player/HPBar.gd
@@ -0,0 +1,16 @@
+extends TextureRect
+var parent_rotation 
+func _ready():
+	$TextureProgress.value=100
+
+func set_percent_value_int(value):
+	$TextureProgress.value=value
+
+
+
+func _process(delta):
+	set_percent_value_int(Globals.player_hp)
+
+
+
+
---
+++ Basic Top Down Shooter/globals/Globals.gd
@@ -0,0 +1,14 @@
+extends Node
+
+
+
+export (int) var player_hp
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	player_hp=100
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
